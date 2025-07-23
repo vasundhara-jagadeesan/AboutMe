4e3388adccc7282fcee982b8e198a17a408c61e3
Vasundhara Jagadeesan <vasundhara.jagadeesan@gmail.com>
2025-07-23 13:39:32 -0400

    Base version with beaver, dog, brownie image and basic text

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -10,10 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Vasundhara")
+                .font(.system(size: 55))
+            HStack{
+                Image("beaver")
+                    .resizable()
+                Text("My hobbies include baking, singing and dance. I've been practicing classical dance since I was little, and also enjoy singing. I")
+            }
+            HStack{
+                Image("Dog")
+                    .resizable()
+                Image("brownies")
+                    .resizable()
+                
+            }
+            Text("That's my dog, Keegan. I love taking walks with him and playing with him. I recently got into baking, and I especially love baking chocolate chip cookies and brownies!")
         }
         .padding()
     }
